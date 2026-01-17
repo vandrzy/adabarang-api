2aba50d512b2c93192620827bbaa55eb74a39ffc
Vandy Rizky Septiawan <vandyrizky33@gmail.com>
2026-01-17 21:40:32 +0800

    feat: initial project

---
+++ src/main/kotlin/com/adabarang/AdaBarangApiApplication.kt
@@ -0,0 +1,11 @@
+package com.adabarang
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AdaBarangApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<AdaBarangApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/adabarang/AdaBarangApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.adabarang
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AdaBarangApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
